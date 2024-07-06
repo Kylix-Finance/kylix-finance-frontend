b2b378902cb1ff510ffdeb80dd3af7455c200eb0
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-07-07 00:00:35 +0330

    feat: add metadata

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+import { metadataGenerator } from "~/config/metadata";
+
+export const metadata: Metadata = metadataGenerator({
+  title: "Dashboard",
+});
+
 export default function page() {
   return <div></div>;
 }
---
+++ apps/web/src/config/metadata.ts
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+
+interface Props {
+  title: string;
+  description?: string;
+}
+
+export const metadataGenerator = ({ title, description }: Props): Metadata => {
+  return {
+    title,
+    description,
+    metadataBase: new URL(process.env.NEXT_PUBLIC_FRONTEND_URL || ""),
+    openGraph: {
+      title,
+      description,
+      url: process.env.NEXT_PUBLIC_FRONTEND_URL || "",
+      siteName: title,
+      images: {
+        url: "/cover.jpg",
+        width: 400,
+        height: 400,
+        type: "image/jpg",
+      },
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: ["/cover.jpg"],
+    },
+    robots: {
+      index: true,
+      follow: true,
+      nocache: false,
+      googleBot: {
+        index: true,
+        follow: true,
+        noimageindex: false,
+      },
+    },
+  };
+};
