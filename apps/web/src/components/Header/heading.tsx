854e23df0239890064e09915569eb204ec9d14e7
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-07-07 10:57:53 +0330

    feat: heading page

---
+++ apps/web/src/components/Header/heading.tsx
@@ -0,0 +1,9 @@
+const Heading = () => {
+  return (
+    <p className="text-lg font-bold leading-5 tracking-[-2%] text-[#1A433B]">
+      Dashboard
+    </p>
+  );
+};
+
+export default Heading;
--- apps/web/src/components/Header/index.tsx
+++ apps/web/src/components/Header/index.tsx
@@ -1,13 +1,22 @@
 import { Box } from "@mui/material";
 import Breadcrumbs from "./breadcrumbs";
+import Heading from "./heading";
 
 const Header = () => {
   return (
     <Box className={"w-full h-[50px] mt-12 mb-4 flex justify-between"}>
-      <div>
+      <Box className="flex flex-col  gap-3">
         <Breadcrumbs />
-      </div>
-      <div>Right</div>
+        <Heading />
+      </Box>
+      <Box className="flex gap-3 h-full items-center text-center">
+        <p className="h-full bg-primary-500 px-2 py-1 text-white items-center text-center flex">
+          User balance
+        </p>
+        <p className="h-full bg-primary-500 px-2 py-1 text-white items-center text-center flex">
+          Connect
+        </p>
+      </Box>
     </Box>
   );
 };
