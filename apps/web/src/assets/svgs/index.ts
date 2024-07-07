1b1b591831265a0c864e89fd1ccfb824d2f7c0b9
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-07-07 13:28:23 +0330

    feat: add social media icons

--- apps/web/src/assets/svgs/index.ts
+++ apps/web/src/assets/svgs/index.ts
@@ -1,5 +1,13 @@
 import WalletIcon from "./wallet.svg";
+import Discord from "./discord.svg";
+import Instagram from "./instagram.svg";
+import Linkedin from "./linkedin.svg";
+import Twitter from "./twitter.svg";
 
 export const Icons = {
   WalletIcon,
+  Discord,
+  Instagram,
+  Linkedin,
+  Twitter,
 };
