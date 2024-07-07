f62ad638bc280d211e5e504f7d824324f23328b5
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-07-07 14:50:57 +0330

    feat: add sidebar footer

--- apps/web/src/components/Sidebar/index.tsx
+++ apps/web/src/components/Sidebar/index.tsx
@@ -1,17 +1,20 @@
 import { Box, Drawer } from "@mui/material";
+import Footer from "./footer";
 
 const Sidebar = () => {
   return (
     <Drawer
       variant="persistent"
       anchor="left"
-      className="w-[290px] h-screen bg-red-500 "
+      className="w-[290px] h-screen"
       open
       PaperProps={{
         component: "div",
         className: "w-[inherit] bg-[inherit] !bg-[inherit]",
       }}
-    ></Drawer>
+    >
+      <Footer />
+    </Drawer>
   );
 };
 export default Sidebar;
