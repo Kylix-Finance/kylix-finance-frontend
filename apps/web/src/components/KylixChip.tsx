a8c5ef1aeb75f76b3773c449d7dcd02666bd2173
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-07-08 12:51:16 +0330

    fix: kylix badge font

--- apps/web/src/components/KylixChip.tsx
+++ apps/web/src/components/KylixChip.tsx
@@ -9,7 +9,9 @@ interface Props {
 const KylixChip = ({ value }: Props) => {
   return (
     <Box className="rounded-[4px] flex px-1.5 py-0.5 bg-primary-400/10 gap-1 items-center justify-center w-fit">
-      <p className="font-bold text-[10px] leading-5 tracking-[-2%] ">{value}</p>
+      <p className="font-bold text-[10px] leading-5 tracking-[-2%] font-number">
+        {value}
+      </p>
       <Image src="kylix-chip.svg" alt="Kylix badge" width={10} height={10} />
     </Box>
   );
