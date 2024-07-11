93d3ead533501674b66b84f7ac919316253c7e11
Arian Hamdi <arian.hamdi@gmail.com>
2024-07-11 04:57:13 +0330

    refactor: dashboard layout

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -19,43 +19,47 @@ export const metadata: Metadata = metadataGenerator({
 const Page = () => {
   return (
     <div className={styles.container}>
-      <Card
-        className={styles.total}
-        title="Total value locked"
-        icon={Icons.WalletFill}
-        hasIconBackground
-      >
-        <TotalLocked />
-      </Card>
-      <Card
-        className={styles.reward}
-        title="Reward imbursed to lenders"
-        icon={Icons.WalletFill}
-      >
-        <Reward />
-      </Card>
-      <Card
-        className={styles.burned}
-        title=" Kylix Burned"
-        icon={Icons.WalletFill}
-      >
-        <Burned />
-      </Card>
-      <Card className={styles.kylix_chart}>
-        <KylixChart />
-      </Card>
-      <Card className={styles.featured} title="Featured Market">
-        <Featured />
-      </Card>
-      <Card className={styles.vault_chart}>
-        <VaultChart />
-      </Card>
-      <Card className={styles.supply} title="Assets to Supply">
-        <Supply />
-      </Card>
-      <Card className={styles.borrow} title="Assets to Borrow">
-        <Borrow />
-      </Card>
+      <div className={styles.top}>
+        <Card
+          className={styles.total}
+          title="Total value locked"
+          icon={Icons.WalletFill}
+          hasIconBackground
+        >
+          <TotalLocked />
+        </Card>
+        <Card
+          className={styles.reward}
+          title="Reward imbursed to lenders"
+          icon={Icons.WalletFill}
+        >
+          <Reward />
+        </Card>
+        <Card
+          className={styles.burned}
+          title=" Kylix Burned"
+          icon={Icons.WalletFill}
+        >
+          <Burned />
+        </Card>
+        <Card className={styles.kylix_chart}>
+          <KylixChart />
+        </Card>
+      </div>
+      <div className={styles.bottom}>
+        <Card className={styles.featured} title="Featured Market">
+          <Featured />
+        </Card>
+        <Card className={styles.vault_chart}>
+          <VaultChart />
+        </Card>
+        <Card className={styles.supply} title="Assets to Supply">
+          <Supply />
+        </Card>
+        <Card className={styles.borrow} title="Assets to Borrow">
+          <Borrow />
+        </Card>
+      </div>
     </div>
   );
 };
