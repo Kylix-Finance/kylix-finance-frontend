a84d490d92328105cbc2755d7ac46fb8b112335d
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-07-11 11:43:41 +0330

    feat: add supply button

--- apps/web/src/app/market/[market-id]/_parts/form/Supply.tsx
+++ apps/web/src/app/market/[market-id]/_parts/form/Supply.tsx
@@ -1,5 +1,17 @@
+import { Button } from "@mui/material";
+
 const Supply = () => {
-  return <div>Supply</div>;
+  return (
+    <div className="flex flex-col gap-6">
+      <Button
+        variant="contained"
+        className="!capitalize !text-white !text-xs !font-semibold !leading-4 !text-center !rounded !py-2 !px-3"
+        size="large"
+      >
+        Supply
+      </Button>
+    </div>
+  );
 };
 
 export default Supply;
