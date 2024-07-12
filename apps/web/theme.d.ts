7bdb04659956b8e9d87dc78eb25598d77b3c375a
SoHeil Vatanpoor <soheilsobeck@gmail.com>
2024-07-13 01:30:11 +0330

    fix(web): `MUI` palette color variants

--- apps/web/theme.d.ts
+++ apps/web/theme.d.ts
@@ -4,20 +4,20 @@ import React from "react";
 //   TypographyPropsVariantOverrides,
 // } from "@mui/material";
 // import { TypographyVariantsOptions } from "@mui/material/styles";
-// import { PaletteColor } from "@mui/material/styles/createPalette";
+import { PaletteColor } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles" {
   interface PaletteColor {
-    // "50": string;
-    // "100": string;
-    // "200": string;
-    // "300": string;
-    // "400": string;
-    // "500": string;
-    // "600": string;
-    // "700": string;
-    // "800": string;
-    // "900": string;
+    "50": string;
+    "100": string;
+    "200": string;
+    "300": string;
+    "400": string;
+    "500": string;
+    "600": string;
+    "700": string;
+    "800": string;
+    "900": string;
   }
   interface TypographyVariantsOptions {}
 }
