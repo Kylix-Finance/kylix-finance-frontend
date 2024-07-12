1add3d1554b39673e0114b73390246e80aa7a498
SoHeil Vatanpoor <soheilsobeck@gmail.com>
2024-07-13 01:25:30 +0330

    feat(web): implement `TBody`

---
+++ apps/web/src/components/Table/tHead.tsx
@@ -0,0 +1,70 @@
+import {
+  Box,
+  TableHead,
+  TableRow,
+  TableRowProps,
+  TableSortLabel,
+} from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
+
+import TCell from "./tCell";
+import { Order, TData, THeadProps } from "./types";
+
+interface Props<K extends string> extends THeadProps {
+  headers: Array<K>;
+  tRowProps?: TableRowProps;
+  disablePadding?: boolean;
+  numeric?: boolean;
+  orderBy: string;
+  order: Order;
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof TData<K>
+  ) => void;
+}
+
+function THead<K extends string>({
+  disablePadding,
+  headers,
+  numeric,
+  onRequestSort,
+  order,
+  orderBy,
+  tRowProps,
+  ...rest
+}: Props<K>) {
+  const createSortHandler =
+    (property: K) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead {...rest}>
+      <TableRow {...tRowProps}>
+        {headers.map((header) => (
+          <TCell
+            key={header}
+            align={numeric ? "right" : "left"}
+            padding={disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === header ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === header}
+              direction={orderBy === header ? order : "asc"}
+              onClick={createSortHandler(header)}
+            >
+              {header}
+              {orderBy === header ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+export default THead;
