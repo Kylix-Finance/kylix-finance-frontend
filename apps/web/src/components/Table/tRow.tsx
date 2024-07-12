3a96e9801396059d425b52721756078433937dfa
SoHeil Vatanpoor <soheilsobeck@gmail.com>
2024-07-13 01:24:56 +0330

    feat(web): implement `TRow`

---
+++ apps/web/src/components/Table/tRow.tsx
@@ -0,0 +1,28 @@
+import { TableCellProps, TableRow } from "@mui/material";
+
+import TCell from "./tCell";
+import { Header, TData, TRowProps } from "./types";
+
+interface Props<K extends string> extends TRowProps {
+  row: TData<K>;
+  headers: Header;
+  tCellProps?: TableCellProps;
+}
+function TRow<K extends string>({
+  headers,
+  row,
+  tCellProps,
+  ...rest
+}: Props<K>) {
+  return (
+    <TableRow {...rest}>
+      {headers.map((header) => (
+        <TCell {...tCellProps} key={header}>
+          {row[header as keyof TData<K>]}
+        </TCell>
+      ))}
+    </TableRow>
+  );
+}
+
+export default TRow;
