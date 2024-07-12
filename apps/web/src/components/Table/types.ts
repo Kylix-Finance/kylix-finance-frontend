afe6ef903c777be18c0c39cdfbb334b67f70244a
SoHeil Vatanpoor <soheilsobeck@gmail.com>
2024-07-13 01:24:18 +0330

    feat(web): implement `Table` props

---
+++ apps/web/src/components/Table/types.ts
@@ -0,0 +1,26 @@
+import {
+  TableBodyTypeMap,
+  TableCellProps,
+  TableContainerProps,
+  TableHeadProps,
+  TableProps,
+  TableRowProps,
+} from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+
+export type TData<K extends string> = Record<
+  K,
+  string | number | React.ReactNode
+>;
+export type Data<K extends string> = Array<TData<K>>;
+export type Header = Array<string>;
+export type TRowProps = TableRowProps;
+export type TBodyProps = OverridableComponent<
+  TableBodyTypeMap<object, "tbody">
+>;
+export type THeadProps = TableHeadProps;
+export type TContainerProps = TableContainerProps;
+export type TBaseProps = TableProps;
+export type TCellProps = TableCellProps;
+
+export type Order = "asc" | "desc";
