d2527d273cf26c8d044ec53927dcea960f217d1b
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-07-14 12:03:01 +0330

    feat: add custom scrollbar

--- apps/web/src/app/_parts/MainLayout.tsx
+++ apps/web/src/app/_parts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import { Sidebar, Header } from "~/components";
+import { ScrollableArea } from "~/components/ScrollableArea";
 
 interface Props {
   children: React.ReactNode;
@@ -9,12 +10,12 @@ const MainLayout = ({ children }: Props) => {
   return (
     <Box className="flex">
       <Sidebar />
-      <Box className="w-full h-screen overflow-y-scroll flex flex-col px-6 ">
+      <ScrollableArea className="w-full h-screen overflow-y-scroll flex flex-col px-6 ">
         <Header />
         <Box component="main" className="w-full flex flex-col">
           {children}
         </Box>
-      </Box>
+      </ScrollableArea>
     </Box>
   );
 };
