7412686d9a092608bc5f847b53416342d844f1c7
Arian Hamdi <arian.hamdi@gmail.com>
2024-07-14 08:09:04 +0330

    feat: implement burned and reward card

--- apps/web/src/app/dashboard/_parts/Burned.tsx
+++ apps/web/src/app/dashboard/_parts/Burned.tsx
@@ -1,9 +1,17 @@
 import { Box } from "@mui/material";
+import { numToLocalString } from "~/utils";
 
 const Burned = () => {
   return (
-    <Box>
-      <h1>Burned</h1>
+    <Box className="flex gap-5">
+      <p className="text-secondary-500">
+        {numToLocalString(302200)}{" "}
+        <span className="ml-2 text-neutral-400">KYL</span>
+      </p>
+      <p>
+        {numToLocalString(514100)}{" "}
+        <span className="ml-2 text-neutral-400">USD</span>
+      </p>
     </Box>
   );
 };
--- apps/web/src/app/dashboard/_parts/Reward.tsx
+++ apps/web/src/app/dashboard/_parts/Reward.tsx
@@ -1,9 +1,17 @@
 import { Box } from "@mui/material";
+import { numToLocalString } from "~/utils";
 
 const Reward = () => {
   return (
-    <Box>
-      <h1>Reward</h1>
+    <Box className="flex gap-5">
+      <p className="text-primary-500">
+        {numToLocalString(20400)}{" "}
+        <span className="ml-2 text-neutral-400">KYL</span>
+      </p>
+      <p>
+        {numToLocalString(82100)}{" "}
+        <span className="ml-2 text-neutral-400">USD</span>
+      </p>
     </Box>
   );
 };
