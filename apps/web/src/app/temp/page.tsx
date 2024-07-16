98e0721008f818dd47bd66c669b3735aeac57790
Arian Hamdi <arian.hamdi@gmail.com>
2024-07-16 08:33:20 +0330

    fix: typescript declaration

--- apps/web/src/app/temp/page.tsx
+++ apps/web/src/app/temp/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { ApiPromise, WsProvider } from "@polkadot/api";
-import { Wallet, useConnect, Button, Modal } from "@repo/wallet-modal";
+import { useConnect, Button, Modal } from "@repo/wallet-modal";
 
 const POLKADOT_WS_PROVIDER = "wss://westend-rpc.polkadot.io";
 
-const wallets: Wallet[] = [
+const wallets = [
   { name: "Fearless Wallet", id: "fearless-wallet" },
   {
     name: "SubWallet - Polkadot Wallet",
@@ -15,7 +15,7 @@ const wallets: Wallet[] = [
     name: "Talisman - Ethereum and Polkadot Wallet",
     id: "talisman",
   },
-  { name: "Enkrypt Crypto Wallet", id: "enkrypt" }
+  { name: "Enkrypt Crypto Wallet", id: "enkrypt" },
 ];
 
 const PolkadotConnection: React.FC = () => {
@@ -49,26 +49,28 @@ const PolkadotConnection: React.FC = () => {
     connectToApi();
   }, []);
 
-  const handleConnect = async (wallet: Wallet) => {
-    const connectionRequest = await connect(wallet);
-    console.log("connectionRequest", connectionRequest);
-    if (connectionRequest && api) {
-      //@ts-expect-error tt
-      const { data: { free: accountBalance } } = await api.query.system.account(connectionRequest);
-      setBalance(accountBalance.toString());
-    }
-  };
-  useEffect(() => {
-    handleConnect(wallets![0]!)
-
-  }, [])
+  // const handleConnect = async (wallet: Wallet) => {
+  //   const connectionRequest = await connect(wallet);
+  //   console.log("connectionRequest", connectionRequest);
+  //   if (connectionRequest && api) {
+  //     //@ts-expect-error tt
+  //     const {
+  //       data: { free: accountBalance },
+  //     } = await api.query.system.account(connectionRequest);
+  //     setBalance(accountBalance.toString());
+  //   }
+  // };
+  // useEffect(() => {
+  //   handleConnect(wallets![0]!);
+  // }, []);
   return (
     <>
       <Modal center />
       <div>
         <h1>Polkadot Westend Testnet</h1>
         <p>
-          Current Block Number: {blockNumber !== null ? blockNumber : "Loading..."}
+          Current Block Number:{" "}
+          {blockNumber !== null ? blockNumber : "Loading..."}
         </p>
         <Button />
         {account && (
