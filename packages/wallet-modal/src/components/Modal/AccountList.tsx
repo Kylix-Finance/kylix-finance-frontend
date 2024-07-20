f7c20c4afeab1ecd88030e84ff32c54d0c2fb5c9
dev-aly3n <aly3n.dev@gmail.com>
2024-07-21 01:36:41 +0330

    fix: modal account list

--- packages/wallet-modal/src/components/Modal/AccountList.tsx
+++ packages/wallet-modal/src/components/Modal/AccountList.tsx
@@ -19,19 +19,22 @@ const AccountList = () => {
 
   return (
     <div className="flex flex-col justify-between gap-4  h-[90%] ">
-      <div className="flex flex-col gap-1.5 overflow-y-scroll">
+      <div className="flex flex-col gap-1.5">
         {accounts?.map((item) => (
           <button
             key={item.address}
-            className={`bg-primary-100/50 hover:bg-primary-500/30 rounded-lg p-4 border-2 transition-colors duration-75 ${
+            className={`flex justify-between gap-4 bg-primary-100/50 hover:bg-primary-500/30 rounded-lg p-4 border-2 transition-colors duration-75 ${
               item.address === activeAccount?.address
                 ? "border-primary-500"
                 : "border-[#F7F7F7]"
             }`}
             onClick={() => handleSelectAccount(item)}
           >
-            <p className="text-[#383E42] font-bold text-sm leading-5">
+            <p className="text-[#383E42] font-bold text-sm leading-5 line-clamp-1 w-24">
               {item.name}
+            </p>
+            <p className="text-[#383E42] font-bold text-sm leading-5 line-clamp-1">
+              {item.address.slice(0, 5)}...{item.address.slice(-5)}
             </p>
           </button>
         ))}
