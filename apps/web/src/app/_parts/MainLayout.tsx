e14b3a62a30ee1f1c913e6be6e0ed203d6956f29
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-07-20 10:14:15 +0330

    fix scrollbar

--- apps/web/src/app/_parts/MainLayout.tsx
+++ apps/web/src/app/_parts/MainLayout.tsx
@@ -1,6 +1,5 @@
 import { Box } from "@mui/material";
 import { Sidebar, Header } from "~/components";
-import { ScrollableArea } from "~/components/ScrollableArea";
 
 interface Props {
   children: React.ReactNode;
@@ -10,12 +9,12 @@ const MainLayout = ({ children }: Props) => {
   return (
     <Box className="flex">
       <Sidebar />
-      <ScrollableArea className="w-full h-screen overflow-y-scroll flex flex-col px-6 ">
+      <Box className="w-full h-screen overflow-y-scroll flex flex-col px-6 ">
         <Header />
         <Box component="main" className="w-full flex flex-col">
           {children}
         </Box>
-      </ScrollableArea>
+      </Box>
     </Box>
   );
 };
