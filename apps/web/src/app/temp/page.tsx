e51fb2ad7d13f29851358d97c344d6fcd4ff18a8
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-07-30 14:11:30 +0330

    fix:

--- apps/web/src/app/temp/page.tsx
+++ apps/web/src/app/temp/page.tsx
@@ -1,173 +1,183 @@
-"use client";
-import {
-  ConnectButton,
-  WalletModal,
-  useDisconnect,
-  useSwitchAccount,
-} from "@repo/wallet-modal";
-import {
-  useActiveAccount,
-  useBalance,
-  useBlockNumber,
-  useRead,
-} from "@repo/onchain-utils";
-import { useProvider } from "@repo/onchain-utils";
-import { useEffect, useState } from "react";
-import { ApiPromise, WsProvider } from "@polkadot/api";
-import {
-  web3Enable,
-  web3Accounts,
-  web3FromAddress,
-} from "@polkadot/extension-dapp";
+// "use client";
+// import {
+//   ConnectButton,
+//   WalletModal,
+//   useDisconnect,
+//   useSwitchAccount,
+// } from "@repo/wallet-modal";
+// import {
+//   useActiveAccount,
+//   useBalance,
+//   useBlockNumber,
+//   useRead,
+// } from "@repo/onchain-utils";
+// import { useProvider } from "@repo/onchain-utils";
+// import { useEffect, useState } from "react";
+// import { ApiPromise, WsProvider } from "@polkadot/api";
+// import {
+//   web3Enable,
+//   web3Accounts,
+//   web3FromAddress,
+// } from "@polkadot/extension-dapp";
 
-const PolkadotConnection = () => {
-  const rpcGHJGKHG = {
-    chainHead: {
-      body: {
-        description: "Get chain head body",
-        params: [
-          {
-            name: "blockHash",
-            type: "Hash",
-          },
-        ],
-        type: "Bytes",
-      },
-    },
-    chainSpec: {
-      chainName: {
-        description: "Get chain name",
-        params: [],
-        type: "Text",
-      },
-    },
-    transactionWatch: {
-      submitAndWatch: {
-        description: "Submit and watch transaction",
-        params: [
-          {
-            name: "extrinsic",
-            type: "Extrinsic",
-          },
-        ],
-        type: "Hash",
-      },
-    },
-  };
+// const PolkadotConnection = () => {
+//   const rpcGHJGKHG = {
+//     chainHead: {
+//       body: {
+//         description: "Get chain head body",
+//         params: [
+//           {
+//             name: "blockHash",
+//             type: "Hash",
+//           },
+//         ],
+//         type: "Bytes",
+//       },
+//     },
+//     chainSpec: {
+//       chainName: {
+//         description: "Get chain name",
+//         params: [],
+//         type: "Text",
+//       },
+//     },
+//     transactionWatch: {
+//       submitAndWatch: {
+//         description: "Submit and watch transaction",
+//         params: [
+//           {
+//             name: "extrinsic",
+//             type: "Extrinsic",
+//           },
+//         ],
+//         type: "Hash",
+//       },
+//     },
+//   };
 
-  const typesjhghjg = {
-    // Define any custom types if required
-    // Example:
-    Auction: {
-      duration: "u32",
-      lease_period_index: "u32",
-    },
-  };
+//   const typesjhghjg = {
+//     // Define any custom types if required
+//     // Example:
+//     Auction: {
+//       duration: "u32",
+//       lease_period_index: "u32",
+//     },
+//   };
 
-  async function createApioooo() {
-    const provider = new WsProvider("wss://westend-rpc.polkadot.io");
+//   async function createApioooo() {
+//     const provider = new WsProvider("wss://westend-rpc.polkadot.io");
 
-    const api = await ApiPromise.create({
-      provider,
-      // rpc:rpc, // Add custom RPC methods
-      // types, // Add custom types if any
-    });
+//     const api = await ApiPromise.create({
+//       provider,
+//       // rpc:rpc, // Add custom RPC methods
+//       // types, // Add custom types if any
+//     });
 
-    // Ensure API is ready
-    await api.isReady;
+//     // Ensure API is ready
+//     await api.isReady;
 
-    return api;
-  }
+//     return api;
+//   }
 
-  // const { api } = useProvider();
-  const { activeAccount } = useActiveAccount();
-  const switchAccount = useSwitchAccount();
-  const [isExtensionEnabled, setExtensionEnabled] = useState(false);
-  const { data } = useRead("getLendingPools", [], true);
-  console.log("data", data);
+//   // const { api } = useProvider();
+//   const { activeAccount } = useActiveAccount();
+//   const switchAccount = useSwitchAccount();
+//   const [isExtensionEnabled, setExtensionEnabled] = useState(false);
+//   const { data } = useRead("getLendingPools", [], true);
+//   console.log("data", data);
 
-  useEffect(() => {
-    const enableExtension = async () => {
-      const extensions = await web3Enable("my-app");
-      if (extensions.length > 0) {
-        setExtensionEnabled(true);
-      } else {
-        console.error(
-          "No extension installed or the user did not accept the authorization"
-        );
-      }
-    };
+//   useEffect(() => {
+//     const enableExtension = async () => {
+//       const extensions = await web3Enable("my-app");
+//       if (extensions.length > 0) {
+//         setExtensionEnabled(true);
+//       } else {
+//         console.error(
+//           "No extension installed or the user did not accept the authorization"
+//         );
+//       }
+//     };
 
-    enableExtension();
-  }, []);
+//     enableExtension();
+//   }, []);
 
-  const switchAccountHandler = () => {
-    switchAccount();
-  };
+//   const switchAccountHandler = () => {
+//     switchAccount();
+//   };
 
-  const { blockNumber } = useBlockNumber();
-  const { balance } = useBalance(activeAccount?.address);
-  const { disconnect } = useDisconnect();
+//   const { blockNumber } = useBlockNumber();
+//   const { balance } = useBalance(activeAccount?.address);
+//   const { disconnect } = useDisconnect();
 
+//   return (
+//     <>
+//       <WalletModal center />
+//       <div className="flex flex-col">
+//         <h1>Polkadot Testnet</h1>
+//         <p>
+//           Current Block Number:{" "}
+//           {blockNumber !== null ? blockNumber : "Loading..."}
+//         </p>
+//         <p>
+//           Current Balance Number: {balance !== null ? balance : "Loading..."}
+//         </p>
+//         <ConnectButton address={activeAccount?.address} />
+//         {activeAccount && (
+//           <div>
+//             <p>Active Account: {JSON.stringify(activeAccount, null, 2)}</p>
+//           </div>
+//         )}
+//         <button onClick={switchAccountHandler}>Switch account</button>
+//         <button onClick={() => disconnect()}>Disconnect</button>
+//         <button
+//           onClick={async () => {
+//             if (!activeAccount) return;
+
+//             // Ensure extension is enabled
+//             if (!isExtensionEnabled) {
+//               console.error("Extension is not enabled");
+//               return;
+//             }
+
+//             const api = await createApioooo();
+//             const extensions = await web3Enable("kylix");
+//             // Get the injector for the active account from the Polkadot.js extension
+//             const injector = await web3FromAddress(activeAccount.address);
+
+//             // Set the signer for the API
+//             api.setSigner(injector.signer);
+
+//             const duration = 1123123;
+//             const leasePeriodIndex = 12;
+
+//             const tx = api?.tx?.auctions?.newAuction?.(
+//               duration,
+//               leasePeriodIndex
+//             );
+
+//             // Sign and send the transaction
+//             const hash = await tx?.signAndSend(activeAccount.address);
+
+//             console.log("Transaction sent with hash", hash?.toJSON());
+//           }}
+//         >
+//           create auction
+//         </button>
+
+//         <div className="flex flex-col gap-4"></div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default PolkadotConnection;
+
+const temp = () => {
   return (
     <>
-      <WalletModal center />
-      <div className="flex flex-col">
-        <h1>Polkadot Testnet</h1>
-        <p>
-          Current Block Number:{" "}
-          {blockNumber !== null ? blockNumber : "Loading..."}
-        </p>
-        <p>
-          Current Balance Number: {balance !== null ? balance : "Loading..."}
-        </p>
-        <ConnectButton address={activeAccount?.address} />
-        {activeAccount && (
-          <div>
-            <p>Active Account: {JSON.stringify(activeAccount, null, 2)}</p>
-          </div>
-        )}
-        <button onClick={switchAccountHandler}>Switch account</button>
-        <button onClick={() => disconnect()}>Disconnect</button>
-        <button
-          onClick={async () => {
-            if (!activeAccount) return;
-
-            // Ensure extension is enabled
-            if (!isExtensionEnabled) {
-              console.error("Extension is not enabled");
-              return;
-            }
-
-            const api = await createApioooo();
-            const extensions = await web3Enable("kylix");
-            // Get the injector for the active account from the Polkadot.js extension
-            const injector = await web3FromAddress(activeAccount.address);
-
-            // Set the signer for the API
-            api.setSigner(injector.signer);
-
-            const duration = 1123123;
-            const leasePeriodIndex = 12;
-
-            const tx = api?.tx?.auctions?.newAuction?.(
-              duration,
-              leasePeriodIndex
-            );
-
-            // Sign and send the transaction
-            const hash = await tx?.signAndSend(activeAccount.address);
-
-            console.log("Transaction sent with hash", hash?.toJSON());
-          }}
-        >
-          create auction
-        </button>
-
-        <div className="flex flex-col gap-4"></div>
-      </div>
+      <p>Temp</p>
     </>
   );
 };
 
-export default PolkadotConnection;
+export default temp;
