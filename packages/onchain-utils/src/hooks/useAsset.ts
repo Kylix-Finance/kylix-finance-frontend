4ab393de4e90923e94ab184754d6dbb17ac7a281
Arian Hamdi <arian.hamdi@gmail.com>
2024-08-04 21:51:36 +0330

    refactor

--- packages/onchain-utils/src/hooks/useAsset.ts
+++ packages/onchain-utils/src/hooks/useAsset.ts
@@ -1,31 +1,31 @@
-import { skipToken, useQuery } from "@tanstack/react-query";
-import { useProvider } from "./useProvider";
-import { queryKeys } from "@repo/shared";
-export const useAsset = (assetId: number) => {
-  const { api } = useProvider();
-  const enabled = !!api;
-
-  return useQuery({
-    queryKey: queryKeys.asset(assetId),
-    queryFn: enabled
-      ? async () => {
-          const assetsInfo = await api?.query?.assets?.asset?.(assetId);
-          return assetsInfo?.toJSON() || {};
-        }
-      : skipToken,
-  });
-};
-
 // import { skipToken, useQuery } from "@tanstack/react-query";
 // import { useProvider } from "./useProvider";
 // import { queryKeys } from "@repo/shared";
-// import { getAsset } from "../api/getAsset";
 // export const useAsset = (assetId: number) => {
 //   const { api } = useProvider();
-//   const enabled = !!api && !!assetId;
+//   const enabled = !!api;
 
 //   return useQuery({
 //     queryKey: queryKeys.asset(assetId),
-//     queryFn: enabled ? () => getAsset({ api, id: assetId }) : skipToken,
+//     queryFn: enabled
+//       ? async () => {
+//           const assetsInfo = await api?.query?.assets?.asset?.(assetId);
+//           return assetsInfo?.toJSON() || {};
+//         }
+//       : skipToken,
 //   });
 // };
+
+import { skipToken, useQuery } from "@tanstack/react-query";
+import { useProvider } from "./useProvider";
+import { queryKeys } from "@repo/shared";
+import { getAsset } from "../api/getAsset";
+export const useAsset = (assetId: number) => {
+  const { api } = useProvider();
+  const enabled = !!api && !!assetId;
+
+  return useQuery({
+    queryKey: queryKeys.asset(assetId),
+    queryFn: enabled ? () => getAsset({ api, id: assetId }) : skipToken,
+  });
+};
