00d7dbe54d721b275579e5c4a08d2e0261fdc8aa
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-08-07 19:18:30 +0330

    feat: add icon generator

--- apps/web/src/components/Icon/index.tsx
+++ apps/web/src/components/Icon/index.tsx
@@ -1,23 +1,36 @@
 "use client";
+import React from "react";
+import { createAvatar } from "@dicebear/core";
+import { glass } from "@dicebear/collection";
 import { data } from "~/config/data";
+import Image from "next/image";
 
 interface Props {
   symbol: string;
-  width?: string | number | undefined;
-  height?: string | number | undefined;
+  width?: number | undefined;
+  height?: number | undefined;
   className?: string;
 }
 
-const Icon = ({
-  symbol,
-  height = "32px",
-  width = "32px",
-  className,
-}: Props) => {
+const Icon = ({ symbol, height = 32, width = 32, className }: Props) => {
   const IconComponent = data.coins[symbol];
 
   if (!IconComponent) {
-    return;
+    const avatar = createAvatar(glass, {
+      size: 128,
+      radius: 100,
+      seed: symbol,
+    }).toDataUri();
+
+    return (
+      <Image
+        src={avatar}
+        alt="Avatar"
+        height={height}
+        width={width}
+        draggable="false"
+      />
+    );
   }
 
   return (
