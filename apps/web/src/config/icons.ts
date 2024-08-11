24f12baf2950d77afd4daf321cf3af0a103fc8d7
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-08-12 00:23:58 +0330

    feat: add new asset icons

--- apps/web/src/config/icons.ts
+++ apps/web/src/config/icons.ts
@@ -1,9 +1,16 @@
 import { Icon } from "~/types";
 import { Coins } from "~/assets/svgs";
-
+import Wbtc from "cryptocurrency-icons/svg/icon/wbtc.svg";
+import Weth from "cryptocurrency-icons/svg/icon/eth.svg";
+import Usdc from "cryptocurrency-icons/svg/icon/usdc.svg";
+import Usdt from "cryptocurrency-icons/svg/icon/usdt.svg";
 export const TokenIcons: Record<string, Icon> = {
   BTC: Coins.BitCoin,
   DOT: Coins.Dot,
   Ankr: Coins.Ankr,
   Aave: Coins.Aave,
+  WBTC: Wbtc,
+  WETH: Weth,
+  USDT: Usdt,
+  USDC: Usdc,
 };
