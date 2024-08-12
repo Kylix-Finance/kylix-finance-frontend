5a062e2a89f6880d69d58d7101f5475e72fd2da2
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-08-12 22:49:28 +0330

    fix: hot fix for deployment

--- apps/web/src/app/markets/page.tsx
+++ apps/web/src/app/markets/page.tsx
@@ -1,16 +1,19 @@
 import { Box } from "@mui/material";
 import Borrow from "./_parts/Borrow";
-import MarketsTable from "./_parts/MarketsTable";
 import Supply from "./_parts/Supply";
+import MarketsTable from "./_parts/MarketsTable";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
-    <Box className="flex flex-col gap-4">
-      <Box className="flex flex-col lg:flex-row gap-4">
-        <Supply />
-        <Borrow />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Box className="flex flex-col gap-4">
+        <Box className="flex flex-col lg:flex-row gap-4">
+          <Supply />
+          <Borrow />
+        </Box>
+        <MarketsTable />
       </Box>
-      <MarketsTable />
-    </Box>
+    </Suspense>
   );
 }
