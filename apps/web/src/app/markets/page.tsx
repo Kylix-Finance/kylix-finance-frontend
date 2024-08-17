f9e78f0599664f5ba7ae6bdb03857def982d58c3
dev-aly3n <aly3n.dev@gmail.com>
2024-08-17 19:33:59 +0330

    fix: markets search param issue

--- apps/web/src/app/markets/page.tsx
+++ apps/web/src/app/markets/page.tsx
@@ -3,17 +3,28 @@ import Borrow from "./_parts/Borrow";
 import Supply from "./_parts/Supply";
 import MarketsTable from "./_parts/MarketsTable";
 import { Suspense } from "react";
+import { Card } from "~/components";
+import { RightComponent } from "./_parts/MarketsTable/RightComponent";
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Box className="flex flex-col gap-4">
-        <Box className="flex flex-col lg:flex-row gap-4">
-          <Supply />
-          <Borrow />
-        </Box>
-        <MarketsTable />
+    <Box className="flex flex-col gap-4">
+      <Box className="flex flex-col lg:flex-row gap-4">
+        <Supply />
+        <Borrow />
       </Box>
-    </Suspense>
+      <Card
+        title="Markets"
+        rightComponent={
+          <Suspense>
+            <RightComponent />
+          </Suspense>
+        }
+      >
+        <Suspense>
+          <MarketsTable />
+        </Suspense>
+      </Card>
+    </Box>
   );
 }
