2fdfc27b831e00fc2225c5b2620f7b20d5683aca
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-09-10 12:17:53 +0330

    fix: grid layout

--- apps/web/src/app/(main-layout)/portfolio/page.tsx
+++ apps/web/src/app/(main-layout)/portfolio/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Skeleton } from "@mui/material";
+import { Box } from "@mui/material";
 import styles from "./_parts/styles.module.css";
 import { Card } from "~/components";
 import { Icons } from "~/assets/svgs";
@@ -10,7 +10,7 @@ import QuickBorrow from "./_parts/QuickBorrow";
 
 export default function Page() {
   return (
-    <Box className={styles.top}>
+    <Box className={styles.container}>
       <Card
         className={styles.total}
         title="Total value"
@@ -30,7 +30,7 @@ export default function Page() {
       <Card className={styles.borrow} hasIconBackground>
         <BorrowValue />
       </Card>
-      <Card className={`${styles.progress_bar}`}>
+      <Card className={styles.progress_bar}>
         <ProgressBar />
       </Card>
       <Card className={styles.quick_borrow} title="Quick Supply">
