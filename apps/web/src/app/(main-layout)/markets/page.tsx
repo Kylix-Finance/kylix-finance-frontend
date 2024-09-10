c96247d4b14a7838bae590990625b0e86c23818a
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-09-10 12:43:56 +0330

    fix: market page

--- apps/web/src/app/(main-layout)/markets/page.tsx
+++ apps/web/src/app/(main-layout)/markets/page.tsx
@@ -5,10 +5,28 @@ import MarketsTable from "./_parts/MarketsTable";
 import { Suspense } from "react";
 import { Card } from "~/components";
 import Search from "./_parts/MarketsTable/Search";
-import { FancyLoader } from "~/components/Loaders";
 import SearchUI from "./_parts/MarketsTable/SearchUI";
 import MarketsTableUI from "./_parts/MarketsTable/MarketsTable";
 
 export default function Page() {
-  return <Box className="flex flex-col gap-4"></Box>;
+  return (
+    <Box className="flex flex-col gap-4">
+      <Box className="flex flex-col lg:flex-row gap-4">
+        <Supply />
+        <Borrow />
+      </Box>
+      <Card
+        title="Markets"
+        rightComponent={
+          <Suspense fallback={<SearchUI />}>
+            <Search />
+          </Suspense>
+        }
+      >
+        <Suspense fallback={<MarketsTableUI />}>
+          <MarketsTable />
+        </Suspense>
+      </Card>
+    </Box>
+  );
 }
