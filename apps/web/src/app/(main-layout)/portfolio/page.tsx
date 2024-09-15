36ba9deddc63de1d17243ca72766ee4a8d0f840a
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-09-15 22:09:55 +0330

    fix: remove extra section

--- apps/web/src/app/(main-layout)/portfolio/page.tsx
+++ apps/web/src/app/(main-layout)/portfolio/page.tsx
@@ -41,9 +41,6 @@ export default function Page() {
       <Card className={styles.borrowed} title="Your Borrows">
         <Borrowed />
       </Card>
-      <Card className={styles.quick_borrow} title="Quick Supply">
-        <QuickBorrow />
-      </Card>
     </Box>
   );
 }
