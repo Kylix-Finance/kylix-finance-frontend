4e8f43979ffc4aea4ddff60498bab396c15a9325
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-09-21 15:12:28 +0330

    feat: position management

--- apps/web/src/app/(main-layout)/srl/[id]/page.tsx
+++ apps/web/src/app/(main-layout)/srl/[id]/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Tvl from "./_parts/Tvl";
+import PositionManagement from "./_parts/PositionManagement";
 export default function Page() {
   return (
-    <Box className="">
+    <Box className="flex flex-col gap-4">
       <Tvl />
+      <PositionManagement />
     </Box>
   );
 }
