0b791dc25203a0f4f85ba875819421c987a4de3a
SoHeil Vatanpoor <soheilsobeck@gmail.com>
2024-09-22 00:28:29 +0330

    feat: add `staking` page icons

--- apps/web/src/assets/svgs/index.ts
+++ apps/web/src/assets/svgs/index.ts
@@ -30,6 +30,12 @@ import WarningAlert from "./warning.svg";
 import World from "./world.svg";
 import XIcon from "./x.svg";
 import Health from "./health.svg";
+import Plus from "./plus.svg";
+import Alpha from "./alpha.svg";
+import Staking1 from "./staking-1.svg";
+import Staking2 from "./staking-2.svg";
+import Staking3 from "./staking-3.svg";
+
 export const sidebarIcons = {
   CrossSwap,
   Dashboard,
@@ -53,20 +59,25 @@ export const Alert = {
 };
 
 export const Icons = {
+  Alpha,
   ArrowRight,
+  ArrowUp,
   Discord,
+  Health,
   Info,
   Instagram,
   KylixLogo,
   LeftArrow,
   Linkedin,
+  Plus: Plus,
   Search,
   Sort,
+  Staking1,
+  Staking2,
+  Staking3,
   Twitter,
   WalletFill,
   World,
   XIcon,
-  ArrowUp,
-  Health,
 };
 export { Coins };
