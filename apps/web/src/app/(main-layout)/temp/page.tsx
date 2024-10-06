04d8e8261cd33a24469d195b50d047880eeb96b6
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-10-06 14:16:20 +0330

    feat: add api hook for kylix price

--- apps/web/src/app/(main-layout)/temp/page.tsx
+++ apps/web/src/app/(main-layout)/temp/page.tsx
@@ -2,28 +2,20 @@
 import { Button } from "@mui/material";
 import { useMetadata } from "@repo/onchain-utils";
 import React, { useEffect } from "react";
+import { useKylixPrice } from "~/hooks/api/useKylixPrice";
 import { useBorrow } from "~/hooks/chain/useBorrow";
 import { usePools } from "~/hooks/chain/usePools";
 
 export default function Page() {
-  const { mutate: borrow, data } = useBorrow();
-  // const { pools } = usePools();
-  // const { assetMetaData } = useMetadata(9)
-  // console.log(assetMetaData);
+  const startDate = "2024-05-01 00:00:00";
+  const endDate = "2024-05-02 00:00:00";
 
-  const handleBorrow = () => {
-    borrow({
-      asset: 9,
-      balance: "1",
-      collateralAsset: 8,
-      collateralBalance: "10",
-    });
-
+  const { data } = useKylixPrice({
+    startDate: "2024-05-01 00:00:00",
+    endDate: "2024-05-02 00:00:00",
+  });
+  useEffect(() => {
     console.log(data);
-  };
-  return (
-    <Button variant="contained" onClick={handleBorrow}>
-      Borrow
-    </Button>
-  );
+  }, []);
+  return <></>;
 }
