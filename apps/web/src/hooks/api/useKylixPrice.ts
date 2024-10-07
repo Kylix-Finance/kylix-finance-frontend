43bc400d0660d57e3897f3fadeb8b13b73bd2bc6
Arian Hamdi <arian.hamdi@gmail.com>
2024-10-07 08:08:25 +0330

    refactor: queryFn in custom hooks

--- apps/web/src/hooks/api/useKylixPrice.ts
+++ apps/web/src/hooks/api/useKylixPrice.ts
@@ -1,6 +1,7 @@
 import { queryKeys } from "@repo/shared";
 import { useQuery } from "@tanstack/react-query";
 import { kylixPriceSchema } from "~/types";
+import axios from "~/lib/axios";
 
 type Params = {
   startDate: string;
@@ -11,15 +12,12 @@ export const useKylixPrice = ({ endDate, startDate }: Params) => {
   return useQuery({
     queryKey: queryKeys.kylixPrice({ endDate, startDate }),
     queryFn: async () => {
-      const response = await fetch(
-        `http://ec2-16-171-18-46.eu-north-1.compute.amazonaws.com:5000/api/kylix_token?start_date=${startDate}&end_date=${endDate}`
-      );
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = (await response.json()) as kylixPriceSchema[];
+      const data = await axios.get<kylixPriceSchema[]>("/kylix_token", {
+        params: {
+          start_date: startDate,
+          end_date: endDate,
+        },
+      });
       return data;
     },
     enabled: !!startDate && !!endDate,
--- apps/web/src/hooks/api/useTotalSupply.ts
+++ apps/web/src/hooks/api/useTotalSupply.ts
@@ -1,6 +1,8 @@
 import { queryKeys } from "@repo/shared";
 import { useQuery } from "@tanstack/react-query";
 import { TotalSupplySchema } from "~/types";
+import axios from "~/lib/axios";
+
 type Params = {
   startDate: string;
   endDate: string;
@@ -10,15 +12,16 @@ export const useTotalSupply = ({ endDate, startDate }: Params) => {
   return useQuery({
     queryKey: queryKeys.totalSupply({ endDate, startDate }),
     queryFn: async () => {
-      const response = await fetch(
-        `http://ec2-16-171-18-46.eu-north-1.compute.amazonaws.com:5000/api/total_supply_borrow?start_date=${startDate}&end_date=${endDate}`
+      const data = await axios.get<TotalSupplySchema[]>(
+        "/total_supply_borrow",
+        {
+          params: {
+            start_date: startDate,
+            end_date: endDate,
+          },
+        }
       );
 
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = (await response.json()) as TotalSupplySchema[];
       return data;
     },
     enabled: !!startDate && !!endDate,
