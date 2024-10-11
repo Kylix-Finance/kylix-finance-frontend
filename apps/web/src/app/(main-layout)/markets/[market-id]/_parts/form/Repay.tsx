26f2fc8fe185296a7b0811fb159d1bb99a1418b5
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-10-11 15:10:13 +0330

    feat: add repay and borrow

--- apps/web/src/app/(main-layout)/markets/[market-id]/_parts/form/Repay.tsx
+++ apps/web/src/app/(main-layout)/markets/[market-id]/_parts/form/Repay.tsx
@@ -1,7 +1,12 @@
 "use client";
-import { ListItem } from "~/components";
+import { ListItem, notify } from "~/components";
 import { Form } from "./Form";
 import { useState } from "react";
+import { useParams } from "next/navigation";
+import { usePool } from "~/hooks/chain/usePool";
+import { parseUnit, useBalance, useMetadata } from "@repo/onchain-utils";
+import { useRepay } from "~/hooks/chain/useRepay";
+import { BASE_ASSET_ID } from "@repo/shared";
 
 const items: Array<ListItem> = [
   {
@@ -34,22 +39,59 @@ const items: Array<ListItem> = [
 ];
 
 export const Repay = () => {
+  const params = useParams();
+  const lendTokenId = params["market-id"] as string;
+  const { pool } = usePool({ assetId: lendTokenId });
   const [value, setValue] = useState("");
-  const onclick = () => {};
+  const { assetMetaData } = useMetadata(lendTokenId);
+  const { mutate, isPending } = useRepay();
+  const { formattedBalance, isLoading: isBalanceLoading } = useBalance({
+    assetId: lendTokenId,
+  });
+  const {
+    formattedBalance: formattedKTokenBalance,
+    isLoading: isFormattedKTokenBalanceLoading,
+  } = useBalance({
+    assetId: pool?.id,
+    customDecimals: assetMetaData?.decimals,
+    enabled: !!assetMetaData && !!pool,
+  });
+  const { assetMetaData: baseAssetMetadata } = useMetadata(BASE_ASSET_ID);
+  const onclick = () => {
+    mutate(
+      {
+        asset: BASE_ASSET_ID,
+        balance: parseUnit(value, Number(baseAssetMetadata?.decimals)),
+        collateralAsset: lendTokenId,
+      },
+      {
+        onSuccess: ({ blockNumber }) => {
+          setValue("");
+          notify({
+            type: "success",
+            title: "Success",
+            message: "Transaction completed on block " + blockNumber,
+          });
+        },
+      }
+    );
+  };
   return (
     <Form
-      assetId={9}
+      assetId={lendTokenId}
       items={items}
-      decimals={18}
-      onMaxClick={() => {}}
+      decimals={assetMetaData?.decimals}
       setValue={setValue}
       value={value}
       submitButton={{
         onclick,
         content: "Repay",
       }}
-      balance=""
-      symbol={undefined}
+      isSubmitting={isPending}
+      isMaxLoading={isBalanceLoading}
+      onMaxClick={() => {}}
+      balance={formattedBalance}
+      symbol={assetMetaData?.symbol}
     />
   );
 };
