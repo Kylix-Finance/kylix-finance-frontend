f0eda9b8bd92d1d81283e42c18122f6348dd43af
dev-aly3n <aly3n.dev@gmail.com>
2024-10-28 01:34:21 +0330

    fix: portfolio borrowed id

--- apps/web/src/app/(main-layout)/markets/_parts/TableActions.tsx
+++ apps/web/src/app/(main-layout)/markets/_parts/TableActions.tsx
@@ -3,36 +3,44 @@ import Link from "next/link";
 
 interface Props {
   assetId: number | string;
+  firstAction?: string;
+  secondActionb?: string;
 }
 
 // TODO: Extract to components
-export const TableActions = ({ assetId }: Props) => {
+export const TableActions = ({
+  assetId,
+  firstAction = "Supply",
+  secondActionb = "Borrow",
+}: Props) => {
   return (
     <Box className="flex justify-end gap-1">
       <Link href={`/markets/${assetId}`}>
         <Button variant="contained">
           <Typography className="!text-[#FFF]" variant="body3" fontWeight={600}>
-            Supply
+            {firstAction}
           </Typography>
         </Button>
       </Link>
-      <Button
-        variant="outlined"
-        sx={{
-          color: "#48484820",
-          borderColor: "#48484820",
-          "&:hover": {
-            borderColor: "#48484820",
-          },
-          "& .MuiTypography-root": {
+      <Link href={`/markets/${assetId}`}>
+        <Button
+          variant="outlined"
+          sx={{
             color: "#48484820",
-          },
-        }}
-      >
-        <Typography variant="body3" fontWeight={600}>
-          Borrow
-        </Typography>
-      </Button>
+            borderColor: "#48484820",
+            "&:hover": {
+              borderColor: "#48484820",
+            },
+            "& .MuiTypography-root": {
+              color: "#48484820",
+            },
+          }}
+        >
+          <Typography variant="body3" fontWeight={600}>
+            {secondActionb}
+          </Typography>
+        </Button>
+      </Link>
     </Box>
   );
 };
