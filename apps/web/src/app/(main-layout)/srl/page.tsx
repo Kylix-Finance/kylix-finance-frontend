c9c67af50293036d37aacbcbc370e34ecf9b94a8
SoHeil Vatanpoor <soheilsobeck@gmail.com>
2024-12-16 19:41:27 +0330

    fix: UI issues

--- apps/web/src/app/(main-layout)/srl/page.tsx
+++ apps/web/src/app/(main-layout)/srl/page.tsx
@@ -6,30 +6,32 @@ import ProgressBar from "~/components/ProgressBar";
 import { PositionsTable } from "./_parts/PositionsTable";
 import { Card } from "~/components";
 import { Connector } from "./_parts/Connector";
+import { Staking1, Staking2, Staking3 } from "~/assets/imgs";
+import Image, { StaticImageData } from "next/image";
 type Item = {
-  iconName: keyof typeof Icons;
+  iconName: StaticImageData;
   title: string;
   description: string;
 };
 
 const arr: Item[] = [
   {
-    iconName: "Staking1",
-    title: "Deposit",
     description:
       "Create a new self repaying loan and deposit one of the supported collateral assets",
+    iconName: Staking1,
+    title: "Deposit",
   },
   {
-    iconName: "Staking2",
-    title: "Convert interest-bearing token",
     description:
       "Convert your deposit into a interest bearing token directly on our LST partner Acala through an integrated seamless process",
+    iconName: Staking2,
+    title: "Convert interest-bearing token",
   },
   {
-    iconName: "Staking3",
-    title: "Borrow and enjoy negative interest",
     description:
       "Borrow token and enjoy your loan with negative interest rates , getting repaid through time.",
+    iconName: Staking3,
+    title: "Borrow and enjoy negative interest",
   },
 ];
 
@@ -62,31 +64,34 @@ export default function Page() {
           <div className="flex gap-6 items-center">
             <Icons.Alpha />
             <div className="flex flex-col gap-2">
-              <h3 className="text-2xl font-bold dark:text-primary-100">
+              <h3 className="text-2xl font-bold text-primary-900 dark:text-primary-100">
                 Borrow with no rate!
               </h3>
-              <p className="text-lg font-light text-primary-100">
+              <p className="text-lg font-light dark:text-primary-100">
                 How self repaying loan works
               </p>
             </div>
           </div>
           <Box className="flex flex-row justify-center">
             {arr.map((item, index) => {
-              const Icon = Icons[item.iconName];
-
               const indexForConnectors = [0, 1];
 
               return (
                 <>
                   <Box className="flex flex-col gap-[10px] w-[214px]">
                     <Box className="flex flex-col gap-[30px]">
-                      <Icon />
+                      <Image
+                        width={215}
+                        height={214}
+                        alt={item.title}
+                        src={item.iconName.src}
+                      />
                       <Typography className="!text-[20px] !text-transparent text-center !bg-clip-text !bg-gradient-to-r !from-[#FFDA8C] !via-[#E6007A] !to-black/20 !font-[700] !leading-[24px] !tracking-[-0.02em]">
                         {item.title}
                       </Typography>
                     </Box>
 
-                    <Typography className="!text-[14px] text-center !text-[#3B434299] !font-[400] !leading-[20px]">
+                    <Typography className="!text-[14px] text-center dark:text-primary-100 !font-[400] !leading-[20px]">
                       {item.description}
                     </Typography>
                   </Box>
--- apps/web/src/assets/imgs/index.ts
+++ apps/web/src/assets/imgs/index.ts
@@ -1,3 +1,6 @@
 import kylixLogoImg from "./kylix-logo.png";
+import Staking1 from "./staking-1.png";
+import Staking2 from "./staking-2.png";
+import Staking3 from "./staking-3.png";
 
-export { kylixLogoImg };
+export { kylixLogoImg, Staking1, Staking2, Staking3 };
