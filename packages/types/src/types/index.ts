d5e40097d8f2a9046e2c58e7eae4ebaa1bf76ec0
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2025-03-09 16:53:52 +0400

    feat: first type

--- packages/types/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export type StoreSetFn<StoreType> = (
-  partial:
-    | StoreType
-    | Partial<StoreType>
-    | ((state: StoreType) => StoreType | Partial<StoreType>),
-  replace?: boolean | undefined
-) => void;
-
-export type VoidNoArgs = () => void;
