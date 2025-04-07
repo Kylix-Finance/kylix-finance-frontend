2c7be2748771e1b1d668713a20d564b2f7e5763f
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2025-04-07 17:26:27 +0400

    feat: add useBalance

---
+++ packages/onchain/src/hooks/query/useBalance.ts
@@ -0,0 +1,82 @@
+"use client";
+
+import { useMetadata } from "./useMetadata";
+import { skipToken, useQuery } from "@tanstack/react-query";
+import { DEFAULT_TOKEN_DECIMALS, queryKeys } from "@repo/shared";
+import { useProvider } from "../useProvider";
+import { useActiveAccount } from "../useActiveAccount";
+import { formatUnit } from "src/utils";
+interface Props {
+  accountAddress?: string;
+  assetId?: number | string;
+  customDecimals?: number;
+  enabled?: boolean;
+}
+
+const useBalance = ({
+  accountAddress,
+  assetId,
+  customDecimals,
+  enabled = true,
+}: Props = {}) => {
+  const { data } = useProvider();
+  const { activeAccount } = useActiveAccount();
+  const address = accountAddress ?? activeAccount?.address;
+  const { data: assetMetaData } = useMetadata(assetId);
+  const finalEnabled = !!data?.api && !!address && enabled;
+
+  return useQuery({
+    queryKey: queryKeys.balance({ address, assetId }),
+    queryFn: finalEnabled
+      ? async () => {
+        const { api } = data
+        if (!api || !address) {
+          throw new Error("API provider or account address is missing.");
+        }
+
+        if (!api.query?.system?.account) {
+          throw new Error(
+            "API provider is not initialized properly or does not support account querying."
+          );
+        }
+
+        let decimals = DEFAULT_TOKEN_DECIMALS;
+        let freeBalance: string;
+
+        if (assetId) {
+          const assetDecimals = customDecimals ?? assetMetaData?.decimals;
+
+          if (!assetDecimals) {
+            throw new Error("Asset metadata is missing.");
+          }
+          decimals = assetDecimals;
+
+          const balanceReq = await api?.query?.assets?.account?.(
+            assetId,
+            address
+          );
+          const result = balanceReq?.toJSON() as unknown as {
+            balance: number | null;
+          };
+
+          freeBalance = BigInt(result?.balance || 0).toString();
+        } else {
+          const result = await api.query.system.account(address);
+          const data = result.toJSON() as any;
+          freeBalance = data.data.free;
+        }
+
+        const freeBalanceBigInt = BigInt(freeBalance);
+        return {
+          formattedBalance: formatUnit(
+            freeBalanceBigInt.toString(),
+            decimals
+          ),
+          realBalance: freeBalanceBigInt,
+        };
+      }
+      : skipToken,
+  });
+};
+
+export { useBalance };
--- packages/onchain/src/utils/index.ts
+++ packages/onchain/src/utils/index.ts
@@ -1,22 +1,5 @@
-import { ApiPromise, WsProvider } from "@polkadot/api";
-
-export const getMetadata = async (
-  endpoint: string = "wss://test-dashboard.kylix.finance"
-) => {
-  let provider: WsProvider | null = null;
-  let api: ApiPromise | null = null;
-
-  try {
-    provider = new WsProvider(endpoint);
-    api = await ApiPromise.create({ provider });
-    const metadata = await api.rpc.state.getMetadata();
-    return metadata.toHex();
-  } catch (error) {
-    console.error("Failed to get metadata:", error);
-    throw error;
-  } finally {
-    if (api) {
-      await api.disconnect();
-    }
-  }
-};
+export { formatUnit } from "./formatUnit";
+export { parseUnit } from "./parseUnit";
+export { fixPrecision } from "./fixPrecision";
+export * from "./format";
+export { formatBigNumbers } from "./formatBigNumbers";
