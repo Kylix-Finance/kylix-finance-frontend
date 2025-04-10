aa9e53ae651dd92f7a8390f3d5809b41b3d5f35d
soheilous <soheilvatanpoor@gmail.com>
2025-04-10 16:01:04 +0330

    fix

---
+++ packages/ui/src/styles/theme.tsx
@@ -0,0 +1,76 @@
+import { fonts } from "../assets/fonts";
+
+const grey = {
+  50: "#F5F7FA",
+  100: "#E9EBEE",
+  200: "#D1D5DB",
+  400: "#697586",
+  600: "#465362",
+  800: "#333333",
+};
+
+const baseTypography = {
+  letterSpacing: "-.3px",
+  color: grey,
+  fontFamily: fonts.inter.style.fontFamily,
+};
+
+// const typography: TypographyVariantsOptions = {
+//   allVariants: baseTypography,
+//   h1: {
+//     fontSize: "40px",
+//     lineHeight: "48px",
+//   },
+//   h2: {
+//     fontSize: "32px",
+//     lineHeight: "40px",
+//   },
+//   h3: {
+//     fontSize: "28px",
+//     lineHeight: "36px",
+//   },
+//   h4: {
+//     fontSize: "24px",
+//     lineHeight: "32px",
+//   },
+//   h5: {
+//     fontSize: "20px",
+//     lineHeight: "28px",
+//   },
+//   h6: {
+//     fontSize: "12px",
+//     lineHeight: "20px",
+//   },
+//   display: {
+//     ...baseTypography,
+//     fontSize: "20px",
+//     lineHeight: "28px",
+//     letterSpacing: "-2%",
+//   },
+//   body: {
+//     ...baseTypography,
+//     fontSize: "18px",
+//     lineHeight: "24px",
+//     letterSpacing: "-2%",
+//   },
+//   bodyCompact: {
+//     ...baseTypography,
+//     fontSize: "16px",
+//     lineHeight: "22px",
+//     letterSpacing: "-2%",
+//   },
+//   bodySmall: {
+//     ...baseTypography,
+//     fontSize: "14px",
+//     lineHeight: "20px",
+//     letterSpacing: "-2%",
+//   },
+//   BTN: {
+//     ...baseTypography,
+//     fontSize: "12px",
+//     lineHeight: "16px",
+//     letterSpacing: "1%",
+//   },
+// };
+
+// const theme = createTheme({ typography });
