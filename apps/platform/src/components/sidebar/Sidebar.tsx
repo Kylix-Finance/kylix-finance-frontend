34d6981f7abdfe50cef87d1b9a30ef5f7d7f9b0f
dev-aly3n <aly3n.dev@gmail.com>
2025-04-20 15:50:00 +0400

    feat: add disabled/link/styles to sidebar

--- apps/platform/src/components/sidebar/Sidebar.tsx
+++ apps/platform/src/components/sidebar/Sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Logo } from "~/assets/svgs";
 import styles from "./Sidebar.module.scss";
 import Home from "~/assets/icons/home";
@@ -7,17 +8,30 @@ import Swap from "~/assets/icons/swap";
 import SwimmingPool from "~/assets/icons/swimming-pool";
 import Coins from "~/assets/icons/coins";
 import Stack from "~/assets/icons/stack";
+import { usePathname } from "next/navigation";
+import Bank from "~/assets/icons/bank";
+import Link from "next/link";
 
-const menuItems = [
-  { icon: Home, label: "Dashboard" },
-  { icon: User, label: "Settings" },
-  { icon: Swap, label: "Profile" },
-  { icon: SwimmingPool, label: "Notifications" },
-  { icon: Coins, label: "Help" },
-  { icon: Stack, label: "Logout" },
+type MenuItem = {
+  path: string;
+  label: string;
+  icon: React.FC<{ className?: string }>;
+  disabled?: boolean;
+};
+
+const menuItems: MenuItem[] = [
+  { path: "/dashboard", label: "Dashboard", icon: Home },
+  { path: "/portfolio", label: "Portfolio", icon: User },
+  { path: "/swap", label: "Swap", icon: Swap },
+  { path: "/pools", label: "Pools", icon: SwimmingPool },
+  { path: "/loans", label: "Loans", icon: Coins },
+  { path: "/stake", label: "Stake", icon: Stack },
+  { path: "/governance", label: "Governance", icon: Bank, disabled: true },
 ];
 
 export const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className={styles.container}>
       <div>
@@ -25,15 +39,17 @@ export const Sidebar = () => {
         <div className={styles.menu}>
           <ul className={styles.menu_list}>
             {menuItems.map((item, index) => (
-              <li
-                key={index}
-                className={clsx(styles.menu_item, {
-                  [styles.active_menu_item]: item.label === "Dashboard",
-                })}
-              >
-                {item.icon && <item.icon className={styles.icon} />}{" "}
-                {item.label}
-              </li>
+              <Link href={item.disabled ? "#" : item.path} key={index}>
+                <li
+                  className={clsx(styles.menu_item, {
+                    [styles.active_menu_item]: pathname === item.path,
+                    [styles.disabled_menu_item]: item.disabled,
+                  })}
+                >
+                  {item.icon && <item.icon className={styles.icon} />}
+                  {item.label}
+                </li>
+              </Link>
             ))}
           </ul>
         </div>
