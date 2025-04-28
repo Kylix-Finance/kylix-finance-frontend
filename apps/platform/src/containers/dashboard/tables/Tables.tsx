64787d68d052c38a28b1000836e2c80c92e67129
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2025-04-28 18:37:46 +0400

    feat: add options

--- apps/platform/src/containers/dashboard/tables/Tables.tsx
+++ apps/platform/src/containers/dashboard/tables/Tables.tsx
@@ -2,20 +2,112 @@ import MarketTable from "../market-table";
 import LiquidationTable from "../liquidation-table";
 import styles from "./Tables.module.scss";
 import { ButtonGroup } from "~/components/ui/button-group";
-import { useState } from "react";
+import { ElementType, useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 import { fadeInOutAnimation, framerProps } from "~/animations/variants";
 import TableLayout from "~/components/table-layout";
 import { Input } from "~/components/ui/input";
 import Search from "~/assets/icons/search.svg";
 import { useQueryState } from "nuqs";
+import { SelectBox } from "~/components/inputs/select-box";
+import Glob from "~/assets/icons/glob.svg";
+import Bitcoin from "~/assets/svgs/crypto-currencies/bitcoin.svg";
+import Dot from "~/assets/svgs/crypto-currencies/dot.svg";
+import capitalize from "lodash/capitalize";
 type Tab = "markets" | "liquidations";
-
+interface Network {
+  name: string;
+  symbol: string;
+  value: string;
+  icon: ElementType;
+}
+interface Sort {
+  name: string;
+  value: string;
+}
+const networks = new Map<string, Network>([
+  [
+    "none",
+    {
+      name: "All Networks",
+      value: "none",
+      symbol: "none",
+      icon: Glob,
+    },
+  ],
+  [
+    "bit",
+    {
+      name: "Bitcoin",
+      value: "bit",
+      symbol: "bitcoin",
+      icon: Bitcoin,
+    },
+  ],
+  [
+    "dot",
+    {
+      name: "Polkadot",
+      value: "dot",
+      symbol: "bitcoin",
+      icon: Dot,
+    },
+  ],
+]);
+const sortOptions = new Map<string, Sort>([
+  [
+    "total_value_locked",
+    {
+      name: "Total Value Locked",
+      value: "total_value_locked",
+    },
+  ],
+  [
+    "none",
+    {
+      name: "None",
+      value: "none",
+    },
+  ],
+  [
+    "most_used_assets",
+    {
+      name: "Most Used Assets",
+      value: "most_used_assets",
+    },
+  ],
+]);
+const networkKeys = Array.from(networks.keys());
+const sortOptionKeys = Array.from(sortOptions.keys());
 export const Tables = () => {
   const [activeTab, setActiveTab] = useState<Tab>("markets");
-  const [q, setQ] = useQueryState("q", {
-    throttleMs: 200,
+
+  const [q, setQ] = useQueryState("q");
+  const [selectedNetwork, setSelectedNetwork] = useQueryState("network", {
+    defaultValue: networkKeys[0],
   });
+  const [selectedSort, setSelectedSort] = useQueryState("sort", {
+    defaultValue: networkKeys[0],
+  });
+
+  const renderNetwork = (key: string) => {
+    const network = networks.get(key);
+    if (!network) return null;
+    return (
+      <div className={styles.selected_network}>
+        <network.icon className={styles.glob} />
+        {capitalize(network.name)}
+      </div>
+    );
+  };
+  const renderSort = (key: string) => {
+    const option = sortOptions.get(key);
+    if (!option) return null;
+    return (
+      <div className={styles.selected_network}>{capitalize(option.name)}</div>
+    );
+  };
+
   return (
     <TableLayout
       header={
@@ -39,17 +131,43 @@ export const Tables = () => {
               }}
             />
           </div>
-          <div
-            style={{
-              width: 300,
-            }}
-          >
-            <Input
-              icon={Search}
-              placeholder="Search by name"
-              onChange={(e) => setQ(e.target.value)}
-              value={q || ""}
-            />
+          <div className={styles.options}>
+            <div className={styles.left}>
+              <div className={styles.option}>
+                <p className={styles.label}>Filter by:</p>
+                <SelectBox
+                  options={networkKeys}
+                  onChange={(value: string) => {
+                    setSelectedNetwork(value);
+                  }}
+                  renderOption={renderNetwork}
+                  value={selectedNetwork}
+                  renderValue={renderNetwork}
+                  className={styles.select}
+                />
+              </div>
+              <div className={styles.option}>
+                <p className={styles.label}>Sort by:</p>
+                <SelectBox
+                  options={sortOptionKeys}
+                  onChange={(value: string) => {
+                    setSelectedSort(value);
+                  }}
+                  renderOption={renderSort}
+                  value={selectedSort}
+                  renderValue={renderSort}
+                  className={styles.select}
+                />
+              </div>
+            </div>
+            <div className={styles.search}>
+              <Input
+                icon={Search}
+                placeholder="Search by name"
+                onChange={(e) => setQ(e.target.value)}
+                value={q || ""}
+              />
+            </div>
           </div>
         </div>
       }
