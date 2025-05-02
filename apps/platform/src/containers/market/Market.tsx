f6437f9189337e26c7634451e0f4d695e4fea55f
dev-aly3n <aly3n.dev@gmail.com>
2025-05-02 16:12:53 +0400

    feat: add scrollable container to market page

--- apps/platform/src/containers/market/Market.tsx
+++ apps/platform/src/containers/market/Market.tsx
@@ -1,6 +1,27 @@
+import styles from "./Market.module.scss";
 
 const Market = () => {
-  return <div>Market</div>;
+  return (
+    <div className={styles.market_container}>
+      <div className={styles.scroll_container}>
+        <div className={styles.slide} style={{ width: "300px" }}>
+          Slide 1
+        </div>
+        <div className={styles.slide} style={{ width: "400px" }}>
+          Slide 2
+        </div>
+        <div className={styles.slide} style={{ width: "350px" }}>
+          Slide 3
+        </div>
+        <div className={styles.slide} style={{ width: "450px" }}>
+          Slide 4
+        </div>
+        <div className={styles.slide} style={{ width: "380px" }}>
+          Slide 5
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Market;
