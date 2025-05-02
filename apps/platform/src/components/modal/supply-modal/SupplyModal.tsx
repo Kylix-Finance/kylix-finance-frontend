33d2231da251eafc06d91ba4a3588c5365d1ac44
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2025-05-02 14:09:03 +0400

    feat: add status component

--- apps/platform/src/components/modal/supply-modal/SupplyModal.tsx
+++ apps/platform/src/components/modal/supply-modal/SupplyModal.tsx
@@ -4,19 +4,23 @@ import styles from "./SupplyModal.module.scss";
 import InputNumber from "~/components/inputs/input-number";
 import GasPump from "~/assets/icons/gas-pump.svg";
 import LogoDot from "~/assets/svgs/logo-dot.svg";
+import Info from "~/assets/icons/info.svg";
+import ArrowRight from "~/assets/icons/arrow-right.svg";
+
 import {
   formatUnit,
   useAssetPrice,
   useBalance,
   useGetLendingPools,
   useSupply,
-  useSimulateSupply,
 } from "@repo/onchain";
-import { ENV } from "~/config/env";
 import { useAccountsStore } from "@repo/shared";
 import { useEffect, useState } from "react";
 import { Button } from "~/components/ui/button";
 import { ExpandableCard } from "~/components/expandable-card";
+import { Status } from "~/components/ui/status";
+import { motion, AnimatePresence } from "motion/react";
+import { fadeInOutAnimation, framerProps } from "~/animations/variants";
 interface Props {
   assetId: number;
   onClose: VoidFunction;
@@ -26,9 +30,6 @@ const SupplyModal = ({ assetId, onClose }: Props) => {
   const [value, setValue] = useState<string | undefined>(undefined);
 
   const { account } = useAccountsStore();
-  const { mutate: simulateSupplyMutate, data: gasFee } = useSimulateSupply({
-    assetId: assetId.toString(),
-  });
 
   const { mutate: supplyMutate } = useSupply({ assetId: assetId?.toString() });
 
@@ -53,14 +54,6 @@ const SupplyModal = ({ assetId, onClose }: Props) => {
     assetId,
     base_asset: null,
   });
-  const {
-    data: kylixAssetPrice,
-    isLoading: isKylixAssetPricePriceLoading,
-    isFetched: isKylixAssetPriceFetched,
-  } = useAssetPrice({
-    assetId: ENV.BASE_ASSET,
-    base_asset: ENV.BASE_ASSET,
-  });
   useEffect(() => {
     if (!assetId || !account) {
       onClose();
@@ -82,9 +75,7 @@ const SupplyModal = ({ assetId, onClose }: Props) => {
 
   const onInputValueChange = async (newValue: string) => {
     setValue(newValue);
-    simulateSupplyMutate({ balance: newValue });
   };
-  console.log("__________kylixAssetPrice", kylixAssetPrice);
 
   return (
     <Modal isOpen={!!assetId} onClose={onClose} title="You’re supplying">
@@ -113,50 +104,136 @@ const SupplyModal = ({ assetId, onClose }: Props) => {
             {value ? "Supply" : "Enter an amount"}
           </Button>
         </div>
-        <ExpandableCard
-          data={[
-            [
-              {
-                title: {
-                  value: `$0.25`,
-                  icon: GasPump,
-                },
-                content: (
-                  <p className={styles.gas_container}>
-                    <span className={styles.logo}>
-                      <LogoDot />
-                    </span>
-                    <span className={styles.price}>0.025 KYL</span>
-                  </p>
-                ),
-              },
-              {
-                title: {
-                  value: "Supply APY",
-                },
-                content: <p>{asset?.supply_apy}</p>,
-              },
-              {
-                title: {
-                  value: "Collateralization",
-                },
-                content: <p className={styles.collateralization}>Enabled</p>,
-              },
-              {
-                title: {
-                  value: "Interest",
-                },
-                content: (
-                  <p className={styles.interest}>
-                    <span>+12 KYL</span>
-                    <span className={styles.interest_usdt}>($24.55)</span>
-                  </p>
-                ),
-              },
-            ],
-            [],
-          ]}
-        />
+        <AnimatePresence mode="wait">
+          {value && (
+            <motion.span {...framerProps} variants={fadeInOutAnimation}>
+              <ExpandableCard
+                data={[
+                  [
+                    {
+                      title: {
+                        value: `$0.25`,
+                        icon: GasPump,
+                      },
+                      content: (
+                        <p className={styles.gas_container}>
+                          <span className={styles.logo}>
+                            <LogoDot />
+                          </span>
+                          <span className={styles.price}>0.025 KYL</span>
+                        </p>
+                      ),
+                    },
+                    {
+                      title: {
+                        value: "Supply APY",
+                      },
+                      content: <p>{asset?.supply_apy}</p>,
+                    },
+                    {
+                      title: {
+                        value: "Collateralization",
+                      },
+                      content: (
+                        <p className={styles.collateralization}>Enabled</p>
+                      ),
+                    },
+                    {
+                      title: {
+                        value: "Interest",
+                      },
+                      content: (
+                        <p className={styles.interest}>
+                          <span>+12 KYL</span>
+                          <span className={styles.interest_usdt}>($24.55)</span>
+                        </p>
+                      ),
+                    },
+                  ],
+                  [
+                    {
+                      title: {
+                        value: "LTV Status",
+                      },
+                      content: (
+                        <div className={styles.ltv}>
+                          <Status
+                            variant="warning"
+                            content="64.9%"
+                            icon={Info}
+                            size="small"
+                            rounded
+                          />
+                          <ArrowRight
+                            width={24}
+                            height={24}
+                            className={styles.arrow}
+                          />
+                          <Status
+                            size="small"
+                            content="44.9%"
+                            icon={Info}
+                            rounded
+                          />
+                        </div>
+                      ),
+                    },
+                    {
+                      title: {
+                        value: "Borrow limit",
+                      },
+                      content: (
+                        <div className={styles.ltv}>
+                          <p>$0.01</p>
+                          <ArrowRight
+                            width={24}
+                            height={24}
+                            className={styles.arrow}
+                          />
+                          <p>$6,20</p>
+                        </div>
+                      ),
+                    },
+                  ],
+                  [
+                    {
+                      title: {
+                        value: "Daily earning",
+                      },
+                      content: (
+                        <div className={styles.ltv}>
+                          <p>$0.01</p>
+                          <ArrowRight
+                            width={24}
+                            height={24}
+                            className={styles.arrow}
+                          />
+                          <p>$6,20</p>
+                        </div>
+                      ),
+                    },
+                    {
+                      title: {
+                        value: `Supply balance (${asset?.asset_symbol})`,
+                      },
+                      content: (
+                        <div className={styles.ltv}>
+                          <p>0</p>
+                          <ArrowRight
+                            width={24}
+                            height={24}
+                            className={styles.arrow}
+                          />
+                          <p>8,276.21</p>
+                        </div>
+                      ),
+                    },
+                  ],
+                ]}
+              />
+            </motion.span>
+          )}
+        </AnimatePresence>
       </div>
     </Modal>
   );
