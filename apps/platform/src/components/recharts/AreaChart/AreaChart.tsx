13226b29eb18ea6ba51e516af2da30a43c1612fb
Arian Hamdi <arian.hamdi@gmail.com>
2025-05-05 19:42:38 +0400

    fix: add horizontal line

--- apps/platform/src/components/recharts/AreaChart/AreaChart.tsx
+++ apps/platform/src/components/recharts/AreaChart/AreaChart.tsx
@@ -18,27 +18,44 @@ import { getShortMonth } from "~/utils/date";
 const CustomDot = (props: DotProps) => {
   const { cx, cy } = props;
   if (cx == null || cy == null) return null;
+
   return (
-    <svg
-      x={cx - 8}
-      y={cy - 8}
-      width={16}
-      height={16}
-      viewBox="0 0 16 16"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        opacity={0.25}
-        d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z"
-        fill="#56DDB4"
+    <svg x={0} y={0} width="100%" height="100%">
+      <line
+        x1="0"
+        y1={cy}
+        x2="100%"
+        y2={cy}
+        stroke="var(--color-neutral-400)"
+        strokeWidth={0.5}
+        strokeDasharray="3 3"
+        style={{ pointerEvents: "none" }}
       />
-      <path
-        d="M8 11.5C9.933 11.5 11.5 9.933 11.5 8C11.5 6.067 9.933 4.5 8 4.5C6.067 4.5 4.5 6.067 4.5 8C4.5 9.933 6.067 11.5 8 11.5Z"
-        fill="#56DDB4"
-        stroke="#1E1E1E"
-        strokeOpacity={0.5}
+
+      <line
+        x1={cx}
+        y1="0"
+        x2={cx}
+        y2="100%"
+        stroke="var(--color-neutral-400)"
+        strokeWidth={0.5}
+        strokeDasharray="3 3"
+        style={{ pointerEvents: "none" }}
       />
+
+      <g transform={`translate(${cx - 8}, ${cy - 8})`}>
+        <path
+          opacity={0.25}
+          d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z"
+          fill="#56DDB4"
+        />
+        <path
+          d="M8 11.5C9.933 11.5 11.5 9.933 11.5 8C11.5 6.067 9.933 4.5 8 4.5C6.067 4.5 4.5 6.067 4.5 8C4.5 9.933 6.067 11.5 8 11.5Z"
+          fill="#56DDB4"
+          stroke="#1E1E1E"
+          strokeOpacity={0.5}
+        />
+      </g>
     </svg>
   );
 };
@@ -47,27 +64,14 @@ const CustomCursor = ({ points, width, height }: CursorProps) => {
   const x = points?.[0]?.x ?? 0;
 
   return (
-    <>
-      <line
-        x1={x}
-        x2={x}
-        y1={0}
-        y2={height}
-        stroke="var(--color-neutral-400)"
-        strokeWidth={0.5}
-        strokeDasharray="3 3"
-      />
-
-      <rect
-        x={x}
-        y={0}
-        width="100%"
-        height="calc(100% - 30px)"
-        fill="rgba(0,0,0,0.4)"
-        pointerEvents="none"
-        // className={styles.rect}
-      />
-    </>
+    <rect
+      x={x}
+      y={0}
+      width="100%"
+      height="calc(100% - 30px)"
+      fill="rgba(0,0,0,0.4)"
+      pointerEvents="none"
+    />
   );
 };
 
