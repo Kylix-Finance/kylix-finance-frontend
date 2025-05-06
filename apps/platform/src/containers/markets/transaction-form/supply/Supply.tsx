d9e2dd2fea424a48fb5ffc1bb2b940e5508207c7
dev-aly3n <aly3n.dev@gmail.com>
2025-05-07 01:18:59 +0400

    feat: add repay/borrow/withdraw tx form

--- apps/platform/src/containers/markets/transaction-form/supply/Supply.tsx
+++ apps/platform/src/containers/markets/transaction-form/supply/Supply.tsx
@@ -7,8 +7,10 @@ import { Row } from "~/components/expandable-card/row";
 import { Divider } from "~/components/divider";
 import { TransactionFormProps } from "~/types";
 import { formatUnit } from "@repo/onchain";
+import { Switch } from "~/components/ui/switch";
 const Supply = ({ data, detail, price, balance }: TransactionFormProps) => {
   const [value, setValue] = useState<string | undefined>(undefined);
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -30,16 +32,23 @@ const Supply = ({ data, detail, price, balance }: TransactionFormProps) => {
       <div className={styles.info}>
         <Row
           title={{ value: "Borrowable Amount", className: styles.row_title }}
-          content="0 BTC"
+          content={`0 ${data?.asset_symbol}`}
+        />
+        <Row
+          title={{ value: "Collateral", className: styles.row_title }}
+          content={<Switch name="collateral" checked={true} />}
         />
         <Divider />
         <Row
-          title={{ value: "Borrow balance (BTC)", className: styles.row_title }}
+          title={{
+            value: `Borrow balance (${data?.asset_symbol})`,
+            className: styles.row_title,
+          }}
           content="0"
         />
         <Row
           title={{ value: "Borrow limit used", className: styles.row_title }}
-          content="0%"
+          content="$0"
         />
         <Row
           title={{ value: "Daily earnings", className: styles.row_title }}
--- apps/platform/src/containers/markets/transaction-form/withdraw/Withdraw.tsx
+++ apps/platform/src/containers/markets/transaction-form/withdraw/Withdraw.tsx
@@ -1,7 +1,58 @@
+"use client";
+import InputNumber from "~/components/inputs/input-number";
 import styles from "./Withdraw.module.scss";
+import { useState } from "react";
+import { PrivateButton } from "~/components/private-button";
+import { Row } from "~/components/expandable-card/row";
+import { Divider } from "~/components/divider";
+import { TransactionFormProps } from "~/types";
+import { formatUnit } from "@repo/onchain";
 
-const Withdraw = () => {
-  return <div className={styles.container}>Withdraw</div>;
+const Withdraw = ({ data, detail, price, balance }: TransactionFormProps) => {
+  const [value, setValue] = useState<string | undefined>(undefined);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <InputNumber
+          title="You're withdrawing"
+          value={value}
+          onChange={(value) => setValue(value)}
+          placeholder="0"
+          price={price && formatUnit(price?.[0], price?.[1])}
+          decimals={data?.asset_decimals}
+          showEstimate
+          availableAmount={formatUnit(
+            balance?.realBalance || 0,
+            data?.asset_decimals
+          )}
+        />
+        <PrivateButton fullWidth>Withdraw</PrivateButton>
+      </div>
+      <div className={styles.info}>
+        <Row
+          title={{ value: "Withdrawable Amount", className: styles.row_title }}
+          content={`0 ${data?.asset_symbol}`}
+        />
+        <Divider />
+        <Row
+          title={{
+            value: `Borrow balance (${data?.asset_symbol})`,
+            className: styles.row_title,
+          }}
+          content="0"
+        />
+        <Row
+          title={{ value: "Borrow limit used", className: styles.row_title }}
+          content="0%"
+        />
+        <Row
+          title={{ value: "Daily earnings", className: styles.row_title }}
+          content="$0"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Withdraw;
