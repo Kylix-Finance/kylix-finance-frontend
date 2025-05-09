f451a4aa1c019298c1d2a47b38282c2296368e7f
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2025-05-09 16:00:22 +0400

    feat: add notfound

--- apps/platform/src/app/auth/login/page.tsx
+++ apps/platform/src/app/auth/login/page.tsx
@@ -1,1 +1,10 @@
-export { default } from "~/containers/auth/login";
+import { notFound } from "next/navigation";
+import { ENV } from "~/config/env";
+import Login from "~/containers/auth/login";
+
+export default function LoginPage() {
+  if (!ENV.IS_PRODUCTION) {
+    return notFound();
+  }
+  return <Login />;
+}
