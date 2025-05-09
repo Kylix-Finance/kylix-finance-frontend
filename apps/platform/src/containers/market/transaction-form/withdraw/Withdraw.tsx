b914b1da8161c8d78b9f907f6a3b0d6cff3ed70f
dev-aly3n <aly3n.dev@gmail.com>
2025-05-09 14:37:49 +0400

    feat: add withdraw modal and form

--- apps/platform/src/containers/market/transaction-form/withdraw/Withdraw.tsx
+++ apps/platform/src/containers/market/transaction-form/withdraw/Withdraw.tsx
@@ -7,7 +7,8 @@ import { Row } from "~/components/expandable-card/row";
 import { Divider } from "~/components/divider";
 import { TransactionFormProps } from "~/types";
 import { formatUnit } from "@repo/onchain";
-
+import { Switch } from "~/components/ui/switch";
+import SupplyModal from "~/components/modal/transactions/supply-modal/SupplyModal";
 const Withdraw = ({
   pool,
   detail,
@@ -16,14 +17,14 @@ const Withdraw = ({
   isLoading,
 }: TransactionFormProps) => {
   const [value, setValue] = useState<string | undefined>(undefined);
-
+  const [isReviewed, setIsReviewed] = useState(false);
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <InputNumber
-          title="You're withdrawing"
+          title="You’re withdrawing"
           value={value}
-          onChange={(value) => setValue(value)}
+          onChange={setValue}
           placeholder="0"
           price={price?.formattedPrice}
           decimals={pool?.asset_decimals}
@@ -34,30 +35,50 @@ const Withdraw = ({
           )}
           isLoading={isLoading}
         />
-        <PrivateButton fullWidth>Withdraw</PrivateButton>
+        <PrivateButton
+          fullWidth
+          onClick={() => setIsReviewed(true)}
+          disabled={!value}
+        >
+          Review
+        </PrivateButton>
       </div>
       <div className={styles.info}>
         <Row
-          title={{ value: "Withdrawable Amount", className: styles.row_title }}
+          title={{ value: "Borrowable Amount", className: styles.row_title }}
           content={`0 ${pool?.asset_symbol}`}
+          isContentLoading={isLoading}
+        />
+        <Row
+          title={{ value: "Collateral", className: styles.row_title }}
+          content={<Switch name="collateral" checked={true} readOnly />}
         />
         <Divider />
         <Row
           title={{
             value: `Borrow balance (${pool?.asset_symbol})`,
             className: styles.row_title,
+            isLoading,
           }}
           content="0"
         />
         <Row
           title={{ value: "Borrow limit used", className: styles.row_title }}
-          content="0%"
+          content="$0"
         />
         <Row
           title={{ value: "Daily earnings", className: styles.row_title }}
           content="$0"
         />
       </div>
+      {pool?.asset_id && isReviewed && (
+        <SupplyModal
+          assetId={pool.asset_id}
+          onClose={() => setIsReviewed(false)}
+          isViewOnly
+          value={value}
+        />
+      )}
     </div>
   );
 };
