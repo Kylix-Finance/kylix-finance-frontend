e32328829c6d0ac0633bf55922260f112ff844da
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2025-05-09 12:52:14 +0400

    feat: add liquidation layout

--- apps/platform/src/containers/liquidation/Liquidation.tsx
+++ apps/platform/src/containers/liquidation/Liquidation.tsx
@@ -1,6 +1,13 @@
 import styles from "./Liquidation.module.scss";
 const Liquidation = () => {
-  return <div className={styles.container}>Liquidation</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>header</div>
+      <div className={styles.chart}>chart</div>
+      <div className={styles.table}>table</div>
+      <div className={styles.form}>form</div>
+    </div>
+  );
 };
 
 export default Liquidation;
