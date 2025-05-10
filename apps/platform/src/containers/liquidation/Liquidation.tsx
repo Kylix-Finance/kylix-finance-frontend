c53aba6552df7a1f269d8276fe8460d1c2ede612
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2025-05-10 10:41:14 +0400

    feat: add data

--- apps/platform/src/containers/liquidation/Liquidation.tsx
+++ apps/platform/src/containers/liquidation/Liquidation.tsx
@@ -4,7 +4,24 @@ const Liquidation = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <ActionHeader data={[]} symbol={["ETH", "BTC"]} />
+        <ActionHeader
+          data={[
+            { content: "Total value locked", value: "$22.59K" },
+            {
+              content: "Pool size",
+              value: "$22,598",
+            },
+            {
+              content: "Max discount",
+              value: "30%",
+            },
+            {
+              content: "Average price",
+              value: "0.00930 BTC",
+            },
+          ]}
+          symbol={["ETH", "BTC"]}
+        />
       </div>
       <div className={styles.chart}>chart</div>
       <div className={styles.table}>table</div>
