3302807180f111c07572e62a87d0fa0ccf75dfb8
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2025-05-11 12:38:52 +0400

    feat:draft

--- apps/platform/src/containers/market/utilization/Utilization.tsx
+++ apps/platform/src/containers/market/utilization/Utilization.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import { ChartContainer } from "~/components/chart-container";
 import UtilizationChart from "~/components/recharts/UtilizationChart";
-
+import { ButtonGroupTab } from "~/types";
+const tabs: ButtonGroupTab[] = [
+  { content: "1M", value: "1m" as const },
+  { content: "6M", value: "6m" as const },
+  { content: "1Y", value: "1y" as const },
+];
 const Utilization = () => {
-  return <UtilizationChart />;
+  const [activeTab, setActiveTab] = useState(tabs[0].value);
+  return (
+    <ChartContainer
+      tabs={tabs}
+      activeTab={activeTab.toString()}
+      onItemClick={() => {}}
+      title=""
+      value={{
+        bigNumStr: "1",
+        decimal: 1,
+      }}
+      hideChildrenOnMobile={false}
+      hideContainerStyle={false}
+    >
+      <UtilizationChart />
+    </ChartContainer>
+  );
 };
 
 export default Utilization;
