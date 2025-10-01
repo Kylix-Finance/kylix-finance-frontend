93c4813dbbbf02e9c455d4691dfbdef1c70b98d8
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2025-10-01 14:54:21 +0400

    feat: add borrow and repay modal

--- apps/platform/src/containers/portfolio/tables/borrowed/Borrowed.tsx
+++ apps/platform/src/containers/portfolio/tables/borrowed/Borrowed.tsx
@@ -2,6 +2,9 @@ import { UseGetAssetWiseBorrowsCollaterals } from "@repo/onchain";
 import { BorrowedTable } from "./table/BorrowedTable";
 import { BREAKPOINTS } from "~/constants";
 import { useViewportSize } from "@mantine/hooks";
+import { useState } from "react";
+import RepayModal from "~/components/modal/transactions/repay-modal/RepayModal";
+import BorrowModal from "~/components/modal/transactions/borrow-modal/BorrowModal";
 export type BorrowedResponse = NonNullable<
   UseGetAssetWiseBorrowsCollaterals["data"]
 >["borrowedAssets"];
@@ -14,17 +17,35 @@ interface Props {
 const Borrowed = ({ data, isEmpty, isPending }: Props) => {
   const { width } = useViewportSize();
   const isDesktop = width >= BREAKPOINTS.DESKTOP;
+  const [selectedRepayAssetId, setSelectedRepayAssetId] = useState<
+    null | number
+  >(null);
+  const [selectedBorrowAssetId, setSelectedBorrowAssetId] = useState<
+    null | number
+  >(null);
   return (
     <div>
       {isDesktop ? (
         <BorrowedTable
           data={data}
           isPending={isPending}
-          onBorrowClick={() => {}}
-          onRepayClick={() => {}}
+          onBorrowClick={(assetId) => setSelectedBorrowAssetId(assetId)}
+          onRepayClick={(assetId) => setSelectedRepayAssetId(assetId)}
         />
       ) : (
         <div></div>
+      )}
+      {selectedRepayAssetId && (
+        <RepayModal
+          assetId={selectedRepayAssetId}
+          onClose={() => setSelectedRepayAssetId(null)}
+        />
+      )}
+      {selectedBorrowAssetId && (
+        <BorrowModal
+          assetId={selectedBorrowAssetId}
+          onClose={() => setSelectedBorrowAssetId(null)}
+        />
       )}
     </div>
   );
