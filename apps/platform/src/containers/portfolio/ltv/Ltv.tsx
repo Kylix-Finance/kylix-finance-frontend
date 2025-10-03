366b9831d4788868cc6139e0d54c82a918527c66
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2025-10-03 12:45:06 +0400

    feat: add skel to ltv

--- apps/platform/src/containers/portfolio/ltv/Ltv.tsx
+++ apps/platform/src/containers/portfolio/ltv/Ltv.tsx
@@ -13,35 +13,45 @@ const Tvl = () => {
       <div className={styles.heading}>
         <p className={styles.title}>Loan-to-Value</p>
         <div className={styles.borders}>
-          <Status variant="success" content="25% (Safe)" />
-          <Status variant="warning" content="50% (Warning)" />
-          <Status
-            variant="error"
-            content={`${formatBigNumbers(formatUnit(data?.liquidation_ltv || 0, 4), 2)}% (Liquidation)`}
-          />
+          <Skeleton isLoading={isPending} width={70} height={25}>
+            <Status variant="success" content="25% (Safe)" />
+          </Skeleton>
+          <Skeleton isLoading={isPending} width={70} height={25}>
+            <Status variant="warning" content="50% (Warning)" />
+          </Skeleton>
+          <Skeleton isLoading={isPending} width={70} height={25}>
+            <Status
+              variant="error"
+              content={`${formatBigNumbers(formatUnit(data?.liquidation_ltv || 0, 4), 2)}% (Liquidation)`}
+            />
+          </Skeleton>
         </div>
       </div>
       <div className={styles.content}>
         <div className={styles.current_container}>
-          <span className={styles.current_value}>
-            {Number(data?.current_ltv || 0)}%
-          </span>
-          <Status
-            variant={
-              Number(formatUnit(data?.current_ltv || 0, 4)) >= 50
-                ? "error"
-                : Number(formatUnit(data?.current_ltv || 0, 4)) >= 25
-                  ? "warning"
-                  : "success"
-            }
-            content={
-              Number(formatUnit(data?.current_ltv || 0, 4)) >= 50
-                ? "Liquidation Risk"
-                : Number(formatUnit(data?.current_ltv || 0, 4)) >= 25
-                  ? "Warning"
-                  : "Safe"
-            }
-          />
+          <Skeleton isLoading={isPending} height={30} width={100}>
+            <span className={styles.current_value}>
+              {Number(data?.current_ltv || 0)}%
+            </span>
+          </Skeleton>
+          <Skeleton isLoading={isPending} width={80} height={25}>
+            <Status
+              variant={
+                Number(formatUnit(data?.current_ltv || 0, 4)) >= 50
+                  ? "error"
+                  : Number(formatUnit(data?.current_ltv || 0, 4)) >= 25
+                    ? "warning"
+                    : "success"
+              }
+              content={
+                Number(formatUnit(data?.current_ltv || 0, 4)) >= 50
+                  ? "Liquidation Risk"
+                  : Number(formatUnit(data?.current_ltv || 0, 4)) >= 25
+                    ? "Warning"
+                    : "Safe"
+              }
+            />
+          </Skeleton>
         </div>
         <Skeleton height={10} isLoading={isPending || !data}>
           {data && (
@@ -54,10 +64,12 @@ const Tvl = () => {
             />
           )}
         </Skeleton>
-        <p className={styles.description}>
-          Current liquidation threshold:{" "}
-          {formatBigNumbers(formatUnit(data?.liquidation_ltv || 0, 4), 2)}%
-        </p>
+        <Skeleton isLoading={isPending} width="80%" height={20}>
+          <p className={styles.description}>
+            Current liquidation threshold:{" "}
+            {formatBigNumbers(formatUnit(data?.liquidation_ltv || 0, 4), 2)}%
+          </p>
+        </Skeleton>
       </div>
     </div>
   );
